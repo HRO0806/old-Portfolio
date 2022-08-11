1edbf58d6d541a5be28269d2f2cfe0470e52b6dd
Henry <henryolson@icloud.com>
2022-08-11 14:50:30 -0500

    Added updated resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,20 +12,18 @@ function About() {
                 <div className={Card} style={{marginTop: '45px'}}>
                     <h2 className={'bio-title'}>Bio</h2>
                     <p className={'bio'}> 
-                    I am a front-end developer from Minnesota who is passionate about 
-                    building sharp and polished user interfaces. I have a deep interest in 
-                    web development, specifically front-end development. I have been 
-                    trained to use tools such as HTML, CSS, JavaScript, Bootstrap, Bulma, 
-                    Node.js, express.js, REACT, and HandleBars (just name a few). Part of 
-                    the reason that I love web development is because I am a naturally 
-                    curious person. I really love front-end development in particular because 
-                    of how it engages both creativity and critical thinking. I am currently 
-                    on track to graduate from the University of Minnesota's coding bootcamp. 
-                    I have learned a lot through this experience and have become both a 
-                    better developer and person because of it. Through this bootcamp 
-                    I have been trained as a full stack developer. I think this sets me 
-                    apart as a frontend developer as it means I can communicate well with 
-                    backend developers.
+                    I am a web developer who is passionate about building sharp and polished 
+                    user interfaces. I have a deep interest in web development, as I am naturally 
+                    curious. I have been trained to use tools such as HTML, CSS, JavaScript, 
+                    Bootstrap, Bulma, Node.js, express.js, REACT, and HandleBars. I have recently 
+                    graduated from the University of Minnesota's coding Bootcamp. I have learned 
+                    a lot through this experience and have become both a better developer and 
+                    person because of it. Through this Bootcamp, I have been trained as a 
+                    full-stack developer. I showed that I am a team player during this program, 
+                    as we had three assignments where we worked as a group. I am also very flexible
+                    and open to learning and trying new things. I heavily value the input of 
+                    others as I think bringing in other perspectives from people that you trust 
+                    creates a better end product.
                     </p>
                 </div>
             </div>
